7fd87711df7c7d6f2001325a0c7797fabed0ebf4
rainc446 <rainc8@nycstudents.net>
2024-09-09 00:24:06 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+public class Main 
+{
+    public static void main(String[] args){
+        System.out.print("Hello World!");
+        System.out.println(" This is my first java program.");
+
+        for (int x = 0; x < 10; x++) {
+            System.out.println(x);
+        }
+        
+    }
+
+}
